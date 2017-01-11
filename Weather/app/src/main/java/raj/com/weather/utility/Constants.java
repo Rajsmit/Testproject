0aeef692860f72202bbdd3861325f9d36ec6e9ed
Rajesh Kumar <rajsmit@gmail.com>
2017-01-11 14:08:44 +0530

    Resolving issue of network error
    The final url had missing parameter q=

--- Weather/app/src/main/java/raj/com/weather/utility/Constants.java
+++ Weather/app/src/main/java/raj/com/weather/utility/Constants.java
@@ -3,9 +3,9 @@ package raj.com.weather.utility;
 public interface Constants {
 
     String appId = "a03ba89e935229a8e01e6ee197ad42e5";
-    String cityId = "q=delhi,in"; //Delhi city by choice
-    String currentTempUrl = "http://api.openweathermap.org/data/2.5/weather?%s" + "&appid=" + appId +"&units=metric";
-    String forcastUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?%s" + "&appid=" + appId +"&units=metric&cnt=5";
+    String cityId = "delhi,in"; //Delhi city by choice
+    String currentTempUrl = "http://api.openweathermap.org/data/2.5/weather?q=%s" + "&appid=" + appId +"&units=metric";
+    String forcastUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s" + "&appid=" + appId +"&units=metric&cnt=5";
     String iconUrl = "http://openweathermap.org/img/w/%s.png";
     String celciusTag = "\u00B0";
     String celciusFullTag = "\u2103";
